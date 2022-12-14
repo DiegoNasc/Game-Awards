116934df6fd415ce4751bfc856b83f9f1cec2b37
DiegoNasc <diegod_dsn@hotmail.com>
2022-12-13 21:24:39 -0300

    Classe de Implementação

---
+++ src/main/java/me/dio/gameawards/service/exception/NoContentException.java
@@ -0,0 +1,9 @@
+package me.dio.gameawards.service.exception;
+
+import java.io.Serial;
+
+public class NoContentException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+}
--- src/main/java/me/dio/gameawards/service/impl/GameServiceImpl.java
+++ src/main/java/me/dio/gameawards/service/impl/GameServiceImpl.java
@@ -3,10 +3,14 @@ package me.dio.gameawards.service.impl;
 import me.dio.gameawards.domain.model.Game;
 import me.dio.gameawards.domain.model.GameRepository;
 import me.dio.gameawards.service.GameService;
+import me.dio.gameawards.service.exception.BusinessException;
+import me.dio.gameawards.service.exception.NoContentException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class GameServiceImpl implements GameService {
@@ -22,21 +26,37 @@ public class GameServiceImpl implements GameService {
 
     @Override
     public Game findById(Long id) {
-        return null;
+        Optional<Game> game = repository.findById(id); //Optional "talvez retorne um jogo"
+        return game.orElseThrow(NoContentException::new);
+
+        /*if (game.isPresent()) {
+            return game.get();
+        } else {
+            throw new NoContentException();
+        }*/
     }
 
     @Override
     public void insert(Game game) {
-
+        if (Objects.nonNull(game.getId())) {
+            throw new BusinessException("O ID do jogo ja existe no banco");
+        }
+        repository.save(game);
     }
 
     @Override
     public void update(Long id, Game game) {
-
+        Game gameDb = findById(id);
+        if (gameDb.getId().equals(game.getId())) {
+            repository.save(game);
+        } else {
+            throw new BusinessException("Os IDs para alteração são divergentes");
+        }
     }
 
     @Override
     public void delete(Long id) {
-
+        Game gameDb = findById(id);
+        repository.delete(gameDb);
     }
 }
